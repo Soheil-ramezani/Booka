515ab42292ffee5cf8e3900a7fdce0bf54bba5bc
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-02-23 13:17:25 +0330

    TopTen Banner style added

---
+++ src/components/mainParts/Top10/TopTen.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="TopTen py-20 px-[15%]">
+    <h1>Top - 10 best selling books</h1>
+    <div class="topBanners mt-20">
+      <div class="topBanner w-fit flex flex-row">
+        <img
+          class="topBanner__img"
+          src="./../../../assets/91hYnazD-oL.jpg"
+          alt=""
+        />
+        <div
+          class="topBanner-texts py-5 pl-4 min-w-[250px] flex flex-col justify-center"
+        >
+          <h2>Book Name</h2>
+          <h3>By: Writer'sName</h3>
+          <h3>$35.66</h3>
+          <button
+            class="mt-5 flex flex-row items-center justify-center p-2 text-center"
+          >
+            Add
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="15"
+              height="15"
+              fill="rgba(255, 255, 255,1)"
+              class="ml-2"
+            >
+              <path
+                d="M7.00488 7.99966V5.99966C7.00488 3.23824 9.24346 0.999664 12.0049 0.999664C14.7663 0.999664 17.0049 3.23824 17.0049 5.99966V7.99966H20.0049C20.5572 7.99966 21.0049 8.44738 21.0049 8.99966V20.9997C21.0049 21.5519 20.5572 21.9997 20.0049 21.9997H4.00488C3.4526 21.9997 3.00488 21.5519 3.00488 20.9997V8.99966C3.00488 8.44738 3.4526 7.99966 4.00488 7.99966H7.00488ZM7.00488 9.99966H5.00488V19.9997H19.0049V9.99966H17.0049V11.9997H15.0049V9.99966H9.00488V11.9997H7.00488V9.99966ZM9.00488 7.99966H15.0049V5.99966C15.0049 4.34281 13.6617 2.99966 12.0049 2.99966C10.348 2.99966 9.00488 4.34281 9.00488 5.99966V7.99966Z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/components/mainParts/main.vue
+++ src/components/mainParts/main.vue
@@ -2,12 +2,14 @@
 import BestSellers from './BestSellers/BestSellers.vue';
 import OnlineBooks from './onlineBooks/OnlineBooks.vue';
 import OffersView from './OffersView/OffersView.vue';
+import TopTen from './Top10/TopTen.vue';
 </script>
 <template>
     <main>
         <BestSellers/>
         <OnlineBooks/>
         <OffersView/>
+        <TopTen/>
     </main>
 </template>
 
