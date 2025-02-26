7f94d21cfc71545f6a9f212025a71f49c610e265
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-02-26 18:42:22 +0330

    Header was responses

--- src/components/HeaderParts/Header/HeaderView.vue
+++ src/components/HeaderParts/Header/HeaderView.vue
@@ -2,12 +2,22 @@
 import Navbar from "../Navbar/Navbar.vue";
 </script>
 <template>
-  <Header class="header  flex flex-col justify-center items-center pb-20">
+  <Header class="header flex flex-col justify-center items-center pb-20">
     <Navbar />
     <div class="header-middle flex flex-row items-center w-full">
       <div
-        class="header-Left w-6/12 pl-[10%] flex flex-col items-start justify-center"
-      >
+        class="header-Left pl-[10%] flex flex-col justify-center"
+        :class="{
+          // bigger
+          'w-6/12': MediaWidth > 990,
+          'items-start': MediaWidth > 990,
+          
+          // smaller
+          'w-full': MediaWidth < 990,
+          'items-center': MediaWidth < 990,
+          
+        }">
+      
         <h1 class="header-title__h1 charm-bold">
           Find Your <br />
           Next Book
@@ -21,16 +31,14 @@ import Navbar from "../Navbar/Navbar.vue";
           Explore Now
         </button>
       </div>
-      <div
-        class="header-right w-6/12 flex flex-row justify-around items-center"
-      >
+      <div class="header-right w-6/12 justify-around items-center" v-show="this.MediaWidth > 990">
         <!-- Books -->
         <div
-          class="header-books w-full pr-[10%] flex flex-row items-center justify-between"
+          class="header-books w-full grid gap-10 items-center justify-center"
         >
           <!-- Right book -->
           <div
-            class="header-book--side h-full flex flex-col pt-20 items-center"
+            class="header-book--sides h-full flex flex-col pt-20 items-center"
           >
             <img
               src="./../../../assets/91hYnazD-oL.jpg"
@@ -40,9 +48,9 @@ import Navbar from "../Navbar/Navbar.vue";
             <h2 class="header-book__h2 text-center mt-4">DAISY DARKER</h2>
             <p class="header-book__p text-center">ALICE FEENY</p>
           </div>
-          <!-- main book -->
+          <!-- middle book -->
           <div
-            class="header-book--main h-full flex flex-col pt-20 items-center"
+            class="header-book--middle h-full flex flex-col pt-20 items-center"
           >
             <h2 class="header-book__h2 text-center mt-4">DAISY DARKER</h2>
             <p class="header-book__p text-center">ALICE FEENY</p>
@@ -54,7 +62,8 @@ import Navbar from "../Navbar/Navbar.vue";
           </div>
           <!-- left -->
           <div
-            class="header-book--side h-full flex flex-col pt-20 items-center"
+            v-show="this.MediaWidth > 1300"
+            class="header-book--sides h-full flex flex-col pt-20 items-center"
           >
             <img
               src="./../../../assets/91hYnazD-oL.jpg"
@@ -73,5 +82,21 @@ import Navbar from "../Navbar/Navbar.vue";
 <script>
 export default {
   name: "HeaderView",
+  data() {
+    return {
+      MediaWidth: window.innerWidth,
+    };
+  },
+  mounted() {
+    window.addEventListener("resize", this.updateMediaWidth);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.updateMediaWidth);
+  },
+  methods: {
+    updateMediaWidth() {
+      this.MediaWidth = window.innerWidth;
+    },
+  },
 };
 </script>
