6de7c46bbee2b4336bbf8c507b79f3e5318375c4
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-03-29 14:52:18 +0330

    mainView deleted and codes tranfer to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,43 @@
 <script setup>
-import HeaderView from '@/components/HeaderParts/Header/HeaderView.vue'
-import mainVue from '@/components/mainParts/main.vue'
-import FooterView from '@/components/Footer/FooterView.vue';
+import HeaderView from "@/components/HeaderParts/Header/HeaderView.vue";
+import FooterView from "@/components/Footer/FooterView.vue";
+import DragScroll from "@/components/dragScroll/dragScroll.vue";
+import OnlineBooks from "@/components/mainParts/onlineBooks/OnlineBooks.vue";
+import OffersView from "@/components/mainParts/OffersView/OffersView.vue";
 </script>
 
 <template>
-<HeaderView/>
-<mainVue/>
-<FooterView/>
+  <div>
+    <HeaderView />
+    <main class="flex flex-col items-center">
+    <!-- BestSellers -->
+    <div class="BestSellers container flex flex-col items-center">
+      <!-- Title -->
+      <div
+        class="partTitle px-[5%] mt-10 flex w-full items-center justify-between"
+      >
+        <h1 class="partTitle__h1 charm-regular">Best Sellers</h1>
+      </div>
+      <!-- DragScroll -->
+      <DragScroll />
+    </div>
+    <OnlineBooks />
+    <OffersView />
+    <!-- Popular -->
+     <div class="PopularBooks container flex flex-col items-center">
+      <!-- Title -->
+    <div
+      class="partTitle px-[5%] mt-10 flex w-full items-center justify-between"
+    >
+      <h1 class="partTitle__h1 charm-regular">Popular Books</h1>
+    </div>
+    <!-- PopularBooks -->
+    <DragScroll/>
+     </div>
+    
+  </main>
+    <FooterView />
+  </div>
 </template>
 
 
