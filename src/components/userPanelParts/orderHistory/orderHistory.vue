7215df0d161714aef2cb6583edc31f776127b6fe
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-08-29 16:41:14 +0330

    get items in orderHistory Function added

--- src/components/userPanelParts/orderHistory/orderHistory.vue
+++ src/components/userPanelParts/orderHistory/orderHistory.vue
@@ -1,7 +1,13 @@
 <template>
  <section class="orderHistory min-h-[60vh] w-5/6 flex items-center justify-center">
-    <div class="cleanHistory flex" v-if="ordersList.length == 0">
+    <div class="cleanHistory flex" v-if="orderList.length == 0" >
         <h3>Your order history is clean ;)</h3>
+    </div>
+    <!--  -->
+    <div v-if="orderList.length !== 0">
+       <div class="shown" v-for="order in orderList" :key="order[0].id">
+        {{ order }}
+       </div>
     </div>
  </section> 
 </template>
@@ -11,10 +17,14 @@ export default {
 name:"orderHistory",
 data(){
     return{
-        ordersList: JSON.parse(localStorage.getItem('OrderList')) || [],
+        orderList:null
     }
 },
-
+beforeMount(){
+    // get OrderList
+     const ordersListInLH=JSON.parse(localStorage.getItem('OrderList')) || [];
+     this.orderList=ordersListInLH
+},
 }
 </script>
 
