8ee9579b5042ed4ca7fc3b3f9d3758a51a824424
Soheil.Ramezani <s.ramezani.official@gmail.com>
2025-10-16 17:26:39 +0330

    userList styled responsive

--- src/components/managerParts/usersList/usersList.vue
+++ src/components/managerParts/usersList/usersList.vue
@@ -2,26 +2,33 @@
   <section
     class="usersList flex justify-center overflow-scroll"
     :class="{
-      'w-5/6 ml-[16.666667%]': MediaSize.MediaWidth >= 660,
-      'w-full': MediaSize.MediaWidth < 660,
+      'w-5/6 ml-[16.666667%]': MediaSize.MediaWidth >= 900,
+      'w-full': MediaSize.MediaWidth < 900,
     }"
   >
     <table class="usersList-table w-[90%] table-auto my-10">
       <thead class="table-header">
         <tr class="table-header__tr">
-          <th class="table-header__th" v-for="header in Headers" :key="header">
-            {{ header }}
-          </th>
+          <th class="table-header__th">id</th>
+          <th class="table-header__th" v-if="MediaSize.MediaWidth >= 400">name</th>
+          <th class="table-header__th">username</th>
+          <th class="table-header__th">phone</th>
+          <th class="table-header__th" v-if="MediaSize.MediaWidth >= 970">Email</th>
+          <th class="table-header__th" v-if="MediaSize.MediaWidth >= 580">address</th>
+          <th class="table-header__th">postal code</th>
+          <th class="table-header__th">delete user</th>
         </tr>
       </thead>
       <tbody class="table-body">
         <tr class="table-body__tr" v-for="user in users" :key="user.id">
           <th class="table-body__th">{{ user.id }}</th>
-          <th class="table-body__th">{{ user.firstName + user.lastName }}</th>
+          <th class="table-body__th" v-if="MediaSize.MediaWidth >= 400">{{ user.firstName + user.lastName }}</th>
           <th class="table-body__th">{{ user.username + user.age }}</th>
           <th class="table-body__th">{{ user.phone }}</th>
-          <th class="table-body__th">{{ user.email }}</th>
-          <th class="table-body__th">
+          <th class="table-body__th" v-if="MediaSize.MediaWidth >= 970">
+            {{ user.email }}
+          </th>
+          <th class="table-body__th" v-if="MediaSize.MediaWidth >= 580">
             {{ user.address.address + "," + user.address.city }}
           </th>
           <th class="table-body__th">{{ user.address.postalCode }}</th>
@@ -42,16 +49,6 @@ export default {
   name: "usersList",
   data() {
     return {
-      Headers: [
-        "num",
-        "name",
-        "username",
-        "phone",
-        "email",
-        "address",
-        "postal code",
-        "delete user",
-      ],
       users: null,
       MediaSize: useMediaStore(),
     };
